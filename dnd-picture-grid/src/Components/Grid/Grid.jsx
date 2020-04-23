b9a5ecc1c525b43302bbd28ee3b4943acbd125a8
Jude Battista <jbattista20@my.whitworth.edu>
2020-04-23 15:42:55 -0700

    Use the styled-components to get Grid.jsx

--- dnd-picture-grid/src/Components/Grid/Grid.jsx
+++ dnd-picture-grid/src/Components/Grid/Grid.jsx
@@ -1,21 +1,37 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
-export function Grid() {
-    return(<div className = 'grid-component'></div>);
-}
+export const Grid = styled.div`
+  width: 600px;
+  display: flex;
+  justify-content: start;
+  flex-wrap: wrap;
+`;
 
+export const GridImage = styled.div`
+  flex-grow: 1;
+  border: 1px solid white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: ${props => `url("${props.src}")`};
+  background-size: cover;
+  background-position: 50%;
+`;
 
-export function GridImage() {
-    return (
-        <div className = 'grid-image-component' 
-            style = {{backgroundImage: props => encodeURI(props.src)}}>
-        </div>
-    );
-}
-
-
-const GridItemWrapper = <div className = 'grid-item-wrapper-container'></div>;
+const GridItemWrapper = styled.div`
+  flex: 0 0 33.3%;
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+  box-sizing: border-box;
+  :before {
+    content: "";
+    display: table;
+    padding-top: 100%;
+  }
+`;
 
 export const GridItem = ({ forwardedRef, ...props }) => (
-    <GridItemWrapper ref={forwardedRef} {...props}/>
+  <GridItemWrapper ref={forwardedRef} {...props} />
 );
